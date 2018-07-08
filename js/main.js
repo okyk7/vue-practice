8f9d792c6e78dd474fe885686d1283723b9a908f
okyk7 <okyk7@users.noreply.github.com>
2018-07-08 18:48:45 +0900

    [update]todo追加実装

--- js/main.js
+++ js/main.js
@@ -10,11 +10,22 @@
     var vm2 = new  Vue({
         el: '#app2',
         data: {
+            newItem: '',
             todos: [
                 'task1',
                 'task2',
                 'task3'
             ]
+        },
+        methods: {
+            // addItem: function(e) {
+                // e.preventDefault(); // html側に.preventつけることでok
+                // this.todods.push(this.newItem);
+            // }
+            addItem: function() {
+                this.todos.push(this.newItem);
+                this.newItem = '';
+            }
         }
     });
 
